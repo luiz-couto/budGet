26ca166fa5ae4817228cb151140d31b00ae820e1
luiz-couto <luiz.felipe.556@outlook.com>
2021-10-24 13:12:28 -0300

    added logbox to ignore native base require cycle warning

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { 
-	View
+	View,
+	LogBox
 } from 'react-native'
 
 import { NativeBaseProvider } from 'native-base'
 
 import AppStyles from './App.scss'
 import Login from './pages/login'
+
+/*
+It seens that native base has a cycle in its dependencies, creating a warning.
+Check if this is still needed in the future
+*/
+LogBox.ignoreLogs([
+	'Require cycle: node_modules/native-base'
+])
 
 const App = () => {
 	return (
