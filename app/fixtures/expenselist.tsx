96ccbc1ee713d96471163df1ca417073f0b7d972
luiz-couto <luiz.felipe.556@outlook.com>
2021-11-08 14:33:30 -0300

    solved not scrolling section list bug in expenseList

--- app/fixtures/expenselist.tsx
+++ app/fixtures/expenselist.tsx
@@ -3,33 +3,131 @@ import CategoryList from './categorylist'
 
 const ExpenseListExample: Expense[] = [
   {
-    name: "Travel Brand",
-    category: CategoryList[1],
-    value: 48.05,
-    date: new Date("08/29/2021"),
-    ignore: false
-  },
-  {
     name: "Delivery Brand",
     category: CategoryList[0],
     value: 32.64,
-    date: new Date("09/30/2021"),
+    date: new Date("09/04/2021"),
     ignore: false
   },
   {
     name: "Cloth Brand",
     category: CategoryList[2],
     value: 16.99,
-    date: new Date("09/30/2021"),
+    date: new Date("09/04/2021"),
     ignore: false
   },
   {
     name: "Streaming Brand",
     category: CategoryList[3],
     value: 16.99,
-    date: new Date("09/28/2021"),
+    date: new Date("09/03/2021"),
     ignore: false
-  }
+  },
+  {
+    name: "Expense Name",
+    category: CategoryList[2],
+    value: 16.99,
+    date: new Date("09/03/2021"),
+    ignore: false
+  },
+  {
+    name: "Expense Name",
+    category: CategoryList[2],
+    value: 16.99,
+    date: new Date("09/03/2021"),
+    ignore: false
+  },
+  {
+    name: "Travel Brand",
+    category: CategoryList[1],
+    value: 48.05,
+    date: new Date("08/29/2021"),
+    ignore: false
+  },
+
+  {
+    name: "Delivery Brand",
+    category: CategoryList[0],
+    value: 32.64,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Cloth Brand",
+    category: CategoryList[2],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Streaming Brand",
+    category: CategoryList[3],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Delivery Brand",
+    category: CategoryList[0],
+    value: 32.64,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Cloth Brand",
+    category: CategoryList[2],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Streaming Brand",
+    category: CategoryList[3],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Delivery Brand",
+    category: CategoryList[0],
+    value: 32.64,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Cloth Brand",
+    category: CategoryList[2],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Streaming Brand",
+    category: CategoryList[3],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },{
+    name: "Delivery Brand",
+    category: CategoryList[0],
+    value: 32.64,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Cloth Brand",
+    category: CategoryList[2],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
+  {
+    name: "Final Brand",
+    category: CategoryList[3],
+    value: 16.99,
+    date: new Date("08/28/2021"),
+    ignore: false
+  },
 ]
 
 export default ExpenseListExample
